6d8208ca2ea8a7c22e17d8a29b8639653d76fb6d
Rosemary Wang <915624+joatmon08@users.noreply.github.com>
2020-12-07 15:19:24 -0500

    Update request to return upstream and http code

--- request.sh
+++ request.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for i in {1..1000}; do curl -s -o /dev/null -w "%{http_code}" localhost:9091; echo ""; sleep 1; done
+for i in {1..1000}; do curl -s localhost:9091 | jq -r '.upstream_calls."http://localhost:9092" | "\(.name),\(.code)"' ; sleep 1; done
